c26d4097a5553966b3221997a0417bca136ffe3f
RezaAlpadizami <reza.alpadizami@moonlay.com>
2024-02-29 18:05:49 +0700

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+// import NewsMain from "./pages/News/news.component";
+import { ConfigProvider } from "antd";
+import Home from "./pages/Home/home.component";
+import PageLayout from "./components/PageLayout/page-layout.component";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              colorPrimary: "#9DB2BF",
+              algorithm: true,
+            },
+            Input: {
+              colorPrimary: "#9DB2BF",
+              algorithm: true,
+            },
+          },
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<PageLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+          {/* <Route path="/people/:id" element={<PageLayout />}>
+            <Route index element={<PeopleDetail />} />
+          </Route>
+          <Route path="/films" element={<PageLayout />}>
+            <Route index element={<Films />} />
+            <Route path="/films/:id" element={<FilmDetail />} />
+          </Route>
+          <Route path="/species" element={<PageLayout />}>
+            <Route index element={<Species />} />
+            <Route path="/species/:id" element={<SpeciesDetail />} />
+          </Route>
+          <Route path="/starships" element={<PageLayout />}>
+            <Route index element={<Starships />} />
+            <Route path="/starships/:id" element={<StarshipDetail />} />
+          </Route>
+          <Route path="/vehicles" element={<PageLayout />}>
+            <Route index element={<Vehicles />} />
+            <Route path="/vehicles/:id" element={<VehicleDetail />} />
+          </Route> */}
+        </Routes>
+      </ConfigProvider>
+    </>
   );
-}
+};
 
 export default App;
